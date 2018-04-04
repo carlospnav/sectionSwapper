b3ae99d35afc01e2ed62a09c363a1bff3998c44e
Carlos P Navarrete Filho <karnav@gmail.com>
2018-04-04 11:22:12 -0300

    Fix double call of moveToTop.

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 /* TODO 
   - Make transitionend run only ONCE per section clicked.
-  - Make returnToMain use THIS instead of APP.
 */
 
 (function(){
@@ -14,13 +13,16 @@
     /*Inicializa o app. */
     init: function initHandler(){
       this.content = document.querySelector("#content > p");
+      this.selectSection = this.selectSection.bind(this);
+      this.returnToMain = this.returnToMain.bind(this);
+      this.moveToTop = this.moveToTop.bind(this);
       document.querySelectorAll(".element")
         .forEach((element) => {
           /* Binda o listener de click a cada seção da página e adiciona
           esta seção ao objecto sections. Como init é rodado somente uma vez, 
           cópias extras de selectSection não serão adicionadas ao elemento através
           do método bind. */
-          element.addEventListener('click', this.selectSection.bind(this));
+          element.addEventListener('click', this.selectSection);
           const newElement = {
             element: element,
             content: element.id.slice(7, 8)
@@ -28,7 +30,14 @@
           this.sections[element.id]= newElement;
         });
     },
+    hasClass: function hasClassHandler(element, className){
+      return (' ' + element.className + ' ').indexOf(' ' + className + ' ') > -1;
+    },
     moveToTop: function moveToTopHandler(e){
+      if (!this.hasClass(e.target, "element")){
+        return false;
+      }
+
       const main = App.current.parentNode;
       main.removeChild(App.current);
       main.prepend(App.current);
@@ -36,7 +45,8 @@
     selectSection: function selectSectionHandler(e){
       this.current = e.target;
 
-      this.current.addEventListener('transitionend', this.moveToTop);
+      console.log("current: ", this.current)
+      this.current.addEventListener('transitionend', this.moveToTop, true);
       this.content.innerHTML = this.sections[this.current.id].content;
       for (item of Object.keys(this.sections)){
         if (e.target.id !== this.sections[item].element.id){
@@ -49,13 +59,13 @@
     },
     returnToMain: function returnToMainHandler(e){
       e.stopPropagation();
-      App.current.removeEventListener('transitionend', this.moveToTop );
-      for(item of Object.keys(App.sections)){
+      this.current.removeEventListener('transitionend', this.moveToTop );
+      for(item of Object.keys(this.sections)){
         App.sections[item].element.classList.remove("contracted");
       }
-      App.current.classList.remove("contracted-to-title");
-      App.current.querySelector("ul").classList.remove("contracted-content");
-      App.title().removeEventListener('click', App.returnToMain);
+      this.current.classList.remove("contracted-to-title");
+      this.current.querySelector("ul").classList.remove("contracted-content");
+      this.title().removeEventListener('click', this.returnToMain);
       this.current = null;
     }
   };
